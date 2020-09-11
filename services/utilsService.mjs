0bcae0893ece58bff4df84b20a2b7e97e08ac08e
Sethlans <developer@mattiagiacone.me>
2020-09-12 00:05:01 +0200

    switched to provider/service structure

--- services/utilsService.mjs
+++ services/utilsService.mjs
@@ -1,5 +1,30 @@
-import redis from '../dal/redis';
 
-const checkProfanity = (msgText,profanityFoundFunction)=>{
- 
+export default (cacheProvider) =>{
+
+    const checkProfanity = (msgText,commID)=>{
+        const profanityArray = cacheProvider.get("badWords",commID);
+        returnprofanityArray.every(function(v) {
+            return msgText.indexOf(v) != -1;
+          });
+
+
+    }
+
+    const addBadWord=(word,commID)=>{
+        const profanityArray = cacheProvider.get("badWords",commID);
+        if(profanityArray===null || profanityArray===undefined)
+            profanityArray=[];
+        if(profanityArray.indexOf(word)===-1){
+            profanityArray.push(word);
+            cacheProvider.set("badWords",commID,profanityArray);
+            return 1;
+        }
+        else
+            return 2;
+    }
+
+    const removeBadWord = (word)=>{
+
+    }
 }
+
